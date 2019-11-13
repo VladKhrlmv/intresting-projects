2c5b51fccde79b9c4d5e5b0e1bd761f174409b8b
VladKhrlmv <kharlamov-vlad03@mail.ru>
2019-11-13 22:57:36 +0300

    main part of the assembly and new project - heart

--- assembly/main.js
+++ assembly/main.js
@@ -3,5 +3,5 @@ var text = document.querySelector('.t');
 window.addEventListener('scroll', function() {
     header.style.opacity = 1 - +window.pageYOffset/700+''; 
     header.style.top = +window.pageYOffset + 'px'; 
-    header.style.backgroundPositionY = - +window.pageYOffset / 2 + 'px'; 
+    header.style.backgroundPositionY = - +window.pageYOffset / 2 + 'px';
 });
